979c4a1f7da616d9af9d7e53e3349c70e7c31a6f
Melissa Tiangtae <melissa_tiangate@cmu.ac.th>
2024-12-03 16:20:17 +0700

    lab7_3_completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,18 +2,39 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	char y;
+
+	if(x == 'A'){
+		y = 'Z';
+	}else{
+
+	if ( x>'A' && x<='Z'){
+
+		y = x-1;
+
+	}else{
+		y = '0';
+	}
+	}
+
+	return y;
 }
 
 int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+
+
+	//cout << before('B');
+	//Test case
+	cout << before('A') << endl;
+	cout << before('B')<< endl;
+	cout << before('P')<< endl;
+	cout << before('T')<< endl;
+	cout << before('Z')<< endl;
+	cout << before('a')<< endl;
+	cout << before('0')<< endl;
+	cout << before('c');
+	
+	return 0 ;
 }
+
+	
